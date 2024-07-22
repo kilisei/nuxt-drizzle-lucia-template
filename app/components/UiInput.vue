eef34055159b34796150f4620b28b704bf05d58d
kilisei <seitzkili+github@gmail.com>
2024-07-22 19:58:59 +0200

    this should be better

--- app/components/UiInput.vue
+++ app/components/UiInput.vue
@@ -4,28 +4,27 @@ const props = defineProps<UiInputProps>()
 interface UiInputProps {
   label: string
   name: string
-  type?: string
-  placeholder?: string
   initialFocused?: boolean
 }
 
 const el = ref<HTMLInputElement | null>(null)
 useFocus(el, { initialValue: props.initialFocused })
 
+const attrs = useAttrs()
+
 const { value, errorMessage } = useField(() => props.name)
 </script>
 
 <template>
   <div class="flex flex-col">
+    {{ attrs }}
     <label :for="label">{{ label }}</label>
     <input
-      v-bind="$attrs"
+      v-bind="attrs"
       :id="label"
       ref="el"
       v-model="value"
       :name
-      :type
-      :placeholder
     >
     <span class="text-red-500">
       {{ errorMessage }}
